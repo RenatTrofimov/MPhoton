caea15b8c0f2e38ab326f04a9e3c2e5f725b5c92
Renat <renatrofimov@gmail.com>
2024-02-29 07:44:50 +0300

    0.01.29.02.1 - completed shceme

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from numba import njit, prange
+from numba import njit, prange, vectorize, float64
 from numba import jit
 import numpy as np
 import time
 
 import matplotlib.pyplot as plt
 ####################################################################
+
+
 @njit(parallel=True)
+
 def calculation(G, data, previousLayer,curentLayer, nextLayer):
     ht = data[0]
     hx = data[1]
@@ -17,21 +20,22 @@ def calculation(G, data, previousLayer,curentLayer, nextLayer):
     hx2 = hx*hx
     hy2 = hy*hy
     hz2 = hz*hz
+    
     for i in prange(1,curentLayer.shape[0]-1):
         for j in prange(1,curentLayer.shape[1]-1):
             for k in prange(1,curentLayer.shape[2]-1):
                 suming = 0.0
                 cur = curentLayer[i,j,k]
-                cur2 = 2.0*cur
+                cur2 = cur+cur
                 for r in prange(G.shape[0]):
                     suming += G[r]*np.sin((r+1)*cur)
-                nextLayer[i,j,k] = cur2
-                - previousLayer[i,j,k]
-                + ht2*(curentLayer[i+1,j,k]-cur2-curentLayer[i-1,j,k])/(hx2)
-                + ht2*(curentLayer[i,j+1,k]-cur2-curentLayer[i,j-1,k])/(hy2)
-                + ht2*(curentLayer[i,j,k+1]-cur2-curentLayer[i,j,k-1])/(hz2)
-                - ht2*F*((cur-previousLayer[i,j,k])/ht)**(2*p-1)
-                - ht2*suming
+                nextValue = (cur2 - previousLayer[i,j,k])/ht2
+                + (curentLayer[i+1,j,k]-cur2+curentLayer[i-1,j,k])/(hx2)
+                + (curentLayer[i,j+1,k]-cur2+curentLayer[i,j-1,k])/(hy2)
+                + (curentLayer[i,j,k+1]-cur2+curentLayer[i,j,k-1])/(hz2)
+                - F*((cur-previousLayer[i,j,k])/ht)**(2*p-1)
+                - suming
+                nextLayer[i,j,k] = nextValue*ht2
     return nextLayer
 @njit(parallel=True)
 def initLayers(initData, data, previousLayer,curentLayer):
@@ -49,12 +53,10 @@ def initLayers(initData, data, previousLayer,curentLayer):
     for i in prange(1,curentLayer.shape[0]-1):
         for j in prange(1,curentLayer.shape[1]-1):
             for k in prange(1,curentLayer.shape[2]-1):
-                previousLayer[i,k,j] = np.exp(-(hz*(k-0.5*kz)/gamma)**2) * np.exp(-b*(hx*(i-0.5*kx)**2)) * np.exp(-b*(hy*(j-0.5*ky)**2))
+                previousLayer[i,j,k] = np.exp(-(hz*(k-0.5*kz)/gamma)**2) * np.exp(-b*(hx*(i-0.5*kx)**2)) * np.exp(-b*(hy*(j-0.5*ky)**2))
+                curentLayer[i,j,k] = np.exp(-(hz*(k-0.5*kz-u*ht)/gamma)**2) * np.exp(-b*(hx*(i-0.5*kx)**2)) * np.exp(-b*(hy*(j-0.5*ky)**2))
     
-    for i in prange(1,curentLayer.shape[0]-1):
-        for j in prange(1,curentLayer.shape[1]-1):
-            for k in prange(1,curentLayer.shape[2]-1):
-                curentLayer[i,k,j] = np.exp(-(hz*(k-0.5*kz-u*ht)/gamma)**2) * np.exp(-b*(hx*(i-0.5*kx)**2)) * np.exp(-b*(hy*(j-0.5*ky)**2))
+                
     
     return previousLayer, curentLayer
 
@@ -66,13 +68,14 @@ initData = np.array([
     0.03,
     0.93,
     np.sqrt(1-0.93**2)
-])
+], dtype=np.float64)
 data = np.array([0.01,
                 0.06,
                 0.06,
                 0.06,
                 0.0001,
-                2.0])
+                2.0
+], dtype=np.float64)
 G = np.array([
                 0.910946143,
                 0.327382235,
@@ -82,21 +85,26 @@ G = np.array([
                 0.056415084,
                 0.040572248,
                 0.029635502,
-                0.021892511])
-curentLayer = np.arange(int(initData[0]*initData[1]*initData[2])).reshape(int(initData[0]), int(initData[1]), int(initData[2]))
-previousLayer = np.arange(int(initData[0]*initData[1]*initData[2])).reshape(int(initData[0]), int(initData[1]), int(initData[2]))
-
+                0.021892511
+], dtype=np.float64)
+curentLayer = np.zeros((int(initData[0]), int(initData[1]), int(initData[2])), dtype=np.float64)
+previousLayer = np.zeros((int(initData[0]), int(initData[1]), int(initData[2])), dtype=np.float64)
+dummyZerosArray = np.zeros((int(initData[0]), int(initData[1]), int(initData[2])), dtype=np.float64)
 ##############################################################################
 
 
 previousLayer, curentLayer = initLayers(initData, data, previousLayer, curentLayer)
-dummyZerosArray = np.zeros((int(initData[0]), int(initData[1]), int(initData[2])))
-ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
-plt.show()
+
+
+print(np.sum(previousLayer))
+print(np.sum(curentLayer))
 for i in range(450):
     start = time.perf_counter()
     previousLayer = calculation(G, data, previousLayer,curentLayer, dummyZerosArray)
     #initLayers.parallel_diagnostics(level=4)
     end = time.perf_counter()
     previousLayer, curentLayer = curentLayer, previousLayer
+    print(np.sum(curentLayer))
     print("Elapsed (after compilation) = {}s".format((end - start)))
+
+
